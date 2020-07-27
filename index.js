a53ed52bf9408f52898c48aa0b2b6f25206c16af
Gowtham <gowtham.p@in.reportbee.com>
2020-07-27 19:54:26 +0530

    POC

--- index.js
+++ index.js
@@ -11,13 +11,27 @@ const server = require('http').createServer(function (req, res) {
 });
 const io = require('socket.io')(server);
 
-
 io.on('connection', client => {
-client.on('connect', () => { console.log('connected'); });
-client.on('event', () => { console.log('running'); });
-client.on('disconnect', () => { console.log('disconnected'); });
+  let state = {
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1
+  };
 
+  client.on('connect', () => { console.log('connected'); });
+
+  client.on('increment', (pos) => { 
+    state[pos] += 1;
+    console.log('New State -> ' + JSON.stringify(state));
+  });
+
+  client.on('getData', (pos) => { 
+    // Do our stuffs
+    client.emit('data', JSON.stringify(state));
+  });
+
+  client.on('disconnect', () => { console.log('disconnected'); });
 });
-
 
 server.listen(3000);
