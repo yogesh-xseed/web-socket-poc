c6b41d684a855a0c9d3913776ba4eedb07d9c6c1
xseed-yogesh <yogesh.sharma@edpulsed.com>
2020-07-28 13:42:18 +0530

    firebase set up

---
+++ firestore/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./config/service-account-cred.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://practice-session-poc.firebaseio.com"
+});
+const db = admin.firestore();
+
+app.listen(3001)
+
+app.post('/postdata', async (req, res) => {
+  let state = { 1: 1, 2: 1, 3: 1, 4: 1};
+  let response = await db.collection('practice-session').add(state);
+  res.send(response.id);
+});
+
+
